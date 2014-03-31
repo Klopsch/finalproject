a59cd977d4eb0e80fae850e3db0d111f60c8a5aa
saasbook <saasbook@saasbook.(none)>
2014-03-31 19:45:26 -0300

    Initialize rails app

---
+++ nationalParks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NationalParks::Application.config.secret_token = '5ff05d14b0c8661715ddca29e558ae2f962440f4158ffea5747cdcb1fd1db110c2f35ebff2a2d3b6acdd2adbac9a8c0dcfc1c6c913fb77873bad490dd14493d9'
