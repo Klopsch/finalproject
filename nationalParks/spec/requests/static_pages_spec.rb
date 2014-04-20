7f7a9a7d690e21a8a867077eac7b490c76ed95da
alch6727 <alice.chien@colorado.edu>
2014-04-20 20:26:49 -0300

    base rspec tests

--- nationalParks/spec/requests/static_pages_spec.rb
+++ nationalParks/spec/requests/static_pages_spec.rb
@@ -7,11 +7,38 @@ describe "Home Page" do
     expect(page).to have_content('Listing parks')
   end
   it "should have the content 'Parkname' " do
-   	visit '/'
+    visit '/'
    	expect(page).to have_content('Parkname')
-   it "should have the content 'Microposts' " do
-   	visit '/'
+  end
+  it "should have the content 'Microposts' " do
+    visit '/'
    	expect(page).to have_content('Microposts')
   end
 end
+
+describe "Sign Up" do
+  it "should have the title 'Sign up'" do
+    visit'/users/sign_up'
+    expect(page).to have_content('Sign up')
+  end
+  it "should have the content 'Email' and 'Password" do
+    visit '/users/sign_up'
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+  end
+end
+
+describe "Sign Up" do
+  it "should have the content 'Sign in'" do
+    visit'/users/sign_in'
+    expect(page).to have_content('Sign in')
+  end
+  it "should have the content 'Email' and 'Password" do
+    visit '/users/sign_in'
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+  end
+end
+
+
 
