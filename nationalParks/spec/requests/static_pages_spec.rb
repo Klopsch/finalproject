3ace6aa99387c0b3dea16d68d9d8dd97aca07b4b
Sarah <sarah.lacoste@colorado.edu>
2014-04-20 16:48:07 -0600

    rspec stuff

--- nationalParks/spec/requests/static_pages_spec.rb
+++ nationalParks/spec/requests/static_pages_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 
 describe "Home Page" do
-  it "should have the content 'Listing Parks'" do
+  it "should have the content 'Listing parks'" do
     visit '/'
-    expect(page).to have_content('Listing Parks')
+    expect(page).to have_content('Listing parks')
+  end
+  it "should have the content 'Parkname' " do
+   	visit '/'
+   	expect(page).to have_content('Parkname')
   end
 end
 
