d7e716cab88202ab8d51bd2ff11d8b9dcb13d70e
alch6727 <alice.chien@colorado.edu>
2014-04-20 20:57:17 -0300

    better spec tests

--- nationalParks/spec/requests/static_pages_spec.rb
+++ nationalParks/spec/requests/static_pages_spec.rb
@@ -16,29 +16,39 @@ describe "Home Page" do
   end
 end
 
+require "spec_helper"
+
 describe "Sign Up" do
-  it "should have the title 'Sign up'" do
-    visit'/users/sign_up'
-    expect(page).to have_content('Sign up')
-  end
-  it "should have the content 'Email' and 'Password" do
-    visit '/users/sign_up'
-    expect(page).to have_content("Email")
-    expect(page).to have_content("Password")
+  it "allows new users to register" do
+    visit "/users/sign_up"
+
+    fill_in "Email",                 :with => "alindeman@example.com"
+    fill_in "Password",              :with => "password", :match => :prefer_exact
+    fill_in "Password confirmation", :with => "password"
+
+    click_button "Sign up"
+
+    page.should have_content("Welcome! You have signed up successfully.")
   end
 end
 
-describe "Sign Up" do
-  it "should have the content 'Sign in'" do
-    visit'/users/sign_in'
-    expect(page).to have_content('Sign in')
-  end
-  it "should have the content 'Email' and 'Password" do
-    visit '/users/sign_in'
-    expect(page).to have_content("Email")
-    expect(page).to have_content("Password")
+
+describe "user sign in" do
+  it "allows users to sign in after they have registered" do
+    user = User.create(:email    => "happy@fake.com",
+                       :password => "password")
+
+    visit "/users/sign_in"
+
+    fill_in "Email",    :with => "happy@fake.com"
+    fill_in "Password", :with => "password"
+
+    click_button "Sign in"
+
+    page.should have_content("Signed in successfully.")
   end
 end
+
 
 
 
