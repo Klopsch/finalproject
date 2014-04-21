193ed5d0c36cd6252d985df0b240f6e02b9b500c
saasbook <saasbook@saasbook.(none)>
2014-04-21 19:55:25 -0300

    Working profiles pages

--- nationalParks/config/routes.rb
+++ nationalParks/config/routes.rb
@@ -1,6 +1,6 @@
 NationalParks::Application.routes.draw do  resources :parks
   resources :microposts
-  #get 'users/:id' => 'users#show'
+  get 'profiles/:id' => 'users#show'
   get "parks/index"
   devise_for :users
   root :to => "parks#index"
