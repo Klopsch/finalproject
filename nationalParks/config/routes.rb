83f94c6299186624eb6f2c264f2ae856cb0abee1
saasbook <saasbook@saasbook.(none)>
2014-04-21 18:52:55 -0300

    Profile page broken

--- nationalParks/config/routes.rb
+++ nationalParks/config/routes.rb
@@ -1,6 +1,6 @@
 NationalParks::Application.routes.draw do  resources :parks
   resources :microposts
-  get 'profile', to: 'users#show'
+  #get 'users/:id' => 'users#show'
   get "parks/index"
   devise_for :users
   root :to => "parks#index"
