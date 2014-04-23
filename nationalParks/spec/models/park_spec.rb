bd0891e49423df48135d798581a1284651f2b90d
saasbook <saasbook@saasbook.(none)>
2014-04-23 19:45:01 -0300

    Beginnings of park spec

--- nationalParks/spec/models/park_spec.rb
+++ nationalParks/spec/models/park_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Park do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory"
+  it "has many microposts"
+  it "is invalid without a parkname"
+  it "is invalid without a location"
 end
