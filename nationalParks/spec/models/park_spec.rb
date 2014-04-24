46b029763f72526e1f143da78845d8158de3b159
alch6727 <alice.chien@colorado.edu>
2014-04-23 21:30:53 -0300

    working base rspec tests for models

--- nationalParks/spec/models/park_spec.rb
+++ nationalParks/spec/models/park_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 
 describe Park do
   it "has a valid factory" do
-    Factory.create(:park).should be_valid
+    FactoryGirl.create(:park).should be_valid
   end
-  it "is invalid without a parkname"
-  it "is invalid without a location"
-  it "has many microposts"
+  it "is invalid without a parkname" do
+  	FactoryGirl.build(:park, parkname: nil).should_not be_valid
+  end
+  it "is invalid without a location"do
+  	FactoryGirl.build(:park, location: nil).should_not be_valid
+  end
+  it "is invalid without information"do
+  	FactoryGirl.build(:park, info: nil).should_not be_valid
+  end
+  it "has many microposts" do
+  	t = Park.reflect_on_association(:microposts)
+  	t.macro.should == :has_many
+  end
 end
--- nationalParks/spec/models/user_spec.rb
+++ nationalParks/spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:user).should be_valid
+  end
+  it "is invalid without email" do
+  	FactoryGirl.build(:user, email: nil).should_not be_valid
+  end
+  it "is invalid without a user"do
+  	FactoryGirl.build(:user, password: nil).should_not be_valid
+  end
+  it "has many microposts" do
+  	t = User.reflect_on_association(:microposts)
+  	t.macro.should == :has_many
+  end
 end
