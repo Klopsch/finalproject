b823db954aa51194e3520ba5a82301dec6b0d4de
saasbook <saasbook@saasbook.(none)>
2014-04-29 22:38:47 -0300

    Adding parks to visited parks denies duplicates

--- nationalParks/app/controllers/users_controller.rb
+++ nationalParks/app/controllers/users_controller.rb
@@ -4,8 +4,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if(params[:add_to_visited_parks] != nil)
-      @user.visited_parks += (", " + params[:add_to_visited_parks])
-      @user.save
+      already_added = false
+      @user.visited_parks.split("; ").each do |park|
+        if(park == params[:add_to_visited_parks])
+          already_added = true
+        end
+      end
+      if(!already_added)
+        if(@user.visited_parks == '')
+          @user.visited_parks += (params[:add_to_visited_parks])
+        else
+          @user.visited_parks += ("; " + params[:add_to_visited_parks])
+        end
+        @user.save
+      end
     end
   end
 
