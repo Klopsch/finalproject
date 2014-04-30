75bc20eccf90d3684d05dc0244a93074891c8fd2
saasbook <saasbook@saasbook.(none)>
2014-04-30 00:26:17 -0300

    Ability to remove from visited parks

--- nationalParks/app/controllers/users_controller.rb
+++ nationalParks/app/controllers/users_controller.rb
@@ -4,21 +4,42 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if(params[:add_to_visited_parks] != nil)
-      already_added = false
-      @user.visited_parks.split("; ").each do |park|
-        if(park == params[:add_to_visited_parks])
-          already_added = true
-        end
-      end
-      if(!already_added)
-        if(@user.visited_parks == '')
-          @user.visited_parks += (params[:add_to_visited_parks])
-        else
-          @user.visited_parks += ("; " + params[:add_to_visited_parks])
-        end
-        @user.save
+      add
+    elsif(params[:remove_from_visited_parks] != nil)
+      remove
+    end
+  end
+
+  def add
+    already_added = false
+    @user.visited_parks.split("; ").each do |park|
+      if(park == params[:add_to_visited_parks])
+        already_added = true
       end
     end
+    if(!already_added)
+      if(@user.visited_parks == '')
+        @user.visited_parks += (params[:add_to_visited_parks])
+      else
+        @user.visited_parks += ("; " + params[:add_to_visited_parks])
+      end
+    @user.save
+    end
+  end
+
+  def remove
+    temp_string = ''
+    @user.visited_parks.split("; ").each do |park|
+      if(park != params[:remove_from_visited_parks])
+        if(temp_string = '')
+          temp_string += park
+        else
+          temp_string += "; " + park
+        end        
+      end
+    end
+    @user.visited_parks = temp_string
+    @user.save
   end
 
 end
