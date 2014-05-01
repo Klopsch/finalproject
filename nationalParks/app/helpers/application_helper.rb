25818e64fc3f812621ce7e8c74c32e90ba1bb518
alch6727 <alice.chien@colorado.edu>
2014-05-01 15:13:38 -0300

    finishing map

--- nationalParks/app/helpers/application_helper.rb
+++ nationalParks/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def avatar_url(park)
+    name_id = park.parkname.gsub(/\s+/, "").downcase
+    "http://intangiblefire.files.wordpress.com/2014/05/#{name_id}5.jpg?w=1100"
+  end
 end
