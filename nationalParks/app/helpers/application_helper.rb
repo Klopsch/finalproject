3dc05e837b7d2f6c9d77bbb0b5143634ba1ab8b4
alch6727 <alice.chien@colorado.edu>
2014-05-01 15:27:33 -0300

    finished maps

--- nationalParks/app/helpers/application_helper.rb
+++ nationalParks/app/helpers/application_helper.rb
@@ -5,6 +5,7 @@ module ApplicationHelper
       index = tag.count.to_f / max.count * (classes.size - 1)
       yield(tag, classes[index.round])
     end
+  end
 	def avatar_url(park)
     name_id = park.parkname.gsub(/\s+/, "").downcase
     "http://intangiblefire.files.wordpress.com/2014/05/#{name_id}5.jpg?w=1100"
